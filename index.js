4e6235b8a9054543407d207b0c44d46d6b37ead9
philiplee13 <philipyjlee95@gmail.com>
2020-11-07 08:15:20 -0800

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 //  quiz object
 function Quiz(questions) {
-    this.jim = 0;
-    this.dwight = 0;
-    this.michael = 0;
+    this.score = 0;
     this.questions = questions;
     this.questionIndex = 0;
 };
@@ -15,7 +13,15 @@ Quiz.prototype.getQuestionIndex = function() {
 
 //  check to see if quiz has ended
 Quiz.prototype.isEnded = function() {
-    return this.questionsIndex === this.questions.length;
+    return this.questionIndex === this.questions.length;
+}
+
+// Quiz add to score for each person
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score ++;
+    }
+    this.questionIndex++;
 }
 
 // function to capture text, choices, answers to populate
@@ -25,24 +31,21 @@ function Question(text, choice, answers) {
     this.answers = answers;
 };
 
-// based on answer, add count to respective character
-Question.prototype.answer = function(choice) {
-    if (choice === "choice-0") {
-        return this.jim++;
-    }
-    else if (choice === "choice-1") {
-        return this.dwight++;
-    }
-    else {
-        return this.michael++;
-    }
-    this.questionIndex++;
+// Save user input into choice
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answers === choice;
 }
-
 // populate the quiz
+// If finished, show the result and the reset button
+// If not, show next question and button
 function populate() {
     if (quiz.isEnded()) {
         showResult();
+        var fullReset = document.getElementById("quiz-reset");
+        fullReset.style.display = "block";
+        fullReset.addEventListener("click",function () {
+            location.reload();
+        }, false);
     }
     else {
         // show the question
@@ -69,8 +72,8 @@ function guess(id,guess) {
         populate();
     }
 };
-// define showProgress();
 
+// define showProgress();
 function showProgress() {
     var currentQuestionNumber = quiz.questionIndex + 1;
     var element = document.getElementById("question-tracker");
@@ -81,35 +84,34 @@ function showProgress() {
 // define showResult();
 
 function showResult() {
-    var highestScore = Math.max(this.jim, this.dwight, this.michael);
-    var quizOver = "<h2>Result</h2>";
-    quizOver += "<h3> Your Score : " + highestScore +"</h3>";
+    var quizOver = "<h2>Final result</h2>";
+    quizOver += "<h3> Your Score : " + quiz.score +"</h3>";
     var element = document.getElementById("quiz")
     element.innerHTML = quizOver;
 };
 
-
-
-
 // Create Questions here
 var questions = [
-    new Question("If you were to pick one of the items at a resturant, which one would you choose?",
-     ["Ham & Cheese Sandwhich",
-     "Beet Salad",
-     "Chicken Breast Hold the Chicken"]),
-    new Question("What's your relationship with your coworkers?", 
-    ["We're all friendly",
-    "Coworkers? These idiots just share the same working space. They'll all bow down to me one day.",
-    "These people? These people are like my best friends, they love me I love them."]),
-    new Question("How would you spend your day off?",
-    ["Take my wife out for a nice relaxing day",
-    "Spend it on my farm preparing for the season",
-    "Oh I don't know, probably scoping out the scene for my next lover, y'know cause I have so many"]),
-    new Question("What's your coffee order?",
-    ["Just a regular coffee with cream",
-    "Coffee? I am a superior human. Coffee is for the weak",
-    "No Coffee just milk and sugar"
-    ])
+    new Question("What did Jim eat once in front of Andy?",
+     ["A tuna sandwich",
+     "A pony sandwich",
+     "Chicken Breast Hold the Chicken"],
+     "A tuna sandwich"),
+    new Question("In the pilot episode, how long did Michael say he was working Dunder Mifflin?", 
+    ["10 years",
+    "15 years",
+    "12 years"],
+    "12 years"),
+    new Question("What does Todd Packer's license plate say?",
+    ["WLHUNG",
+    "TODPACK",
+    "BIGB@LLS"],
+    "WLHUNG"),
+    new Question("What item of Andy's did Jim put in jello?",
+    ["A mug",
+    "A stapler",
+    "A calculator"],
+    "A calculator")
 ];
 
 // create quiz
